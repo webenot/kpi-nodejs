1d0bd317dbf515246dd1762efc11e7e53b2a78c1
Halyna Urbanska <webenot@mail.ua>
2020-08-15 18:51:45 +0300

    Live Reload

---
+++ liveReload/lib/submodule1.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const method1 = () => 1111;
+const method2 = () => 12;
+
+module.exports = {
+  method1,
+  method2,
+};
---
+++ liveReload/lib/submodule3.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const method31 = () => 31;
+const method32 = () => 32;
+
+module.exports = {
+  method31,
+  method32,
+};
