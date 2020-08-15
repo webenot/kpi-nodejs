334341745ca9fb55ef727d0cfdbace66324e5012
Halyna Urbanska <webenot@mail.ua>
2020-08-15 12:49:00 +0300

    Buffer

---
+++ buffer/2-from.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const b1 = Buffer.from([ 1, 2, 3, 4, 5, 6 ]);
+console.log(b1);
+
+const b2 = Buffer.from('Marcus Aurelius (Марк Аврелий)');
+
+console.log('hex', b2.toString('hex'));
+console.log('base64', b2.toString('base64'));
+console.log('utf8', b2.toString('utf8'));
+console.log('binary', b2.toString('binary'));
---
+++ buffer/5-iterate.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const buffer = Buffer.from('Marcus');
+
+for (const char of buffer.values()) {
+  console.dir({ char });
+}
+
+for (const [ index, code ] of buffer.entries()) {
+  const char = String.fromCharCode(code);
+  console.dir({
+    index,
+    code,
+    char,
+  });
+}
