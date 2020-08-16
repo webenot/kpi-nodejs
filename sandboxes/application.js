9adce9c5768ce51e6d0a4a2709a425b03405dc58
Halyna Urbanska <webenot@mail.ua>
2020-08-16 10:27:43 +0300

    Sandboxes

---
+++ sandboxes/application.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// File contains a small piece of the source to demonstrate main module
+// of a sample application to be executed in the sandboxed context by
+// another pice of code from `framework.js`. Read README.md for tasks.
+
+const fs = require('fs');
+const net = require('net');
+
+// Print from the global context of application module
+console.log('From application global context');
+console.dir({
+  fs,
+  net,
+}, { depth: 1 });
+console.dir({ global }, { depth: 1 });
+// eslint-disable-next-line no-undef
+console.dir({ api }, { depth: 2 });
+
+module.exports = () => {
+  // Print from the exported function context
+  console.log('From application exported function');
+};
